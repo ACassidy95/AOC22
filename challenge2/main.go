e30eaf25ac542c7f8ffe2bac453b71ceaad7ea72
aaronc <aaron.cassidy1995@gmail.com>
2022-12-11 21:55:58 +0000

    Add mappings for moves to game states and calculation of points based off move and state

--- challenge2/main.go
+++ challenge2/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 const (
@@ -13,7 +15,7 @@ const (
 )
 
 const (
-	LOSS = 3 * (iota - 1)
+	LOSS = 3 * iota
 	DRAW
 	WIN
 )
@@ -27,6 +29,12 @@ var move_mapping = map[string]int{
 	"Z": SCISSORS,
 }
 
+var rule_mapping = map[int][]int{
+	ROCK:     {DRAW, LOSS, WIN},
+	PAPER:    {WIN, DRAW, LOSS},
+	SCISSORS: {LOSS, WIN, DRAW},
+}
+
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -34,9 +42,26 @@ func main() {
 		os.Exit(1)
 	}
 	score := calculateScore(file)
+	file.Close()
 	fmt.Printf("Total score: %d\n", score)
 }
 
 func calculateScore(F *os.File) int {
+	scanner := bufio.NewScanner(F)
+	totalPoints := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		moves := strings.Split(line, " ")
 
+		// Add 1 to each hand's points since move constants start at 0
+		totalPoints += calculateTurnPoints(moves[1], moves[0]) + 1
+	}
+	return totalPoints
+}
+
+func calculateTurnPoints(m1, m2 string) int {
+	myMove := move_mapping[m1]
+	oppMove := move_mapping[m2]
+	myState := rule_mapping[myMove][oppMove]
+	return myMove + myState
 }
