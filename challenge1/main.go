666bfeeaa280553e841f48385437dcbad7ec5d82
aaronc <aaron.cassidy1995@gmail.com>
2022-12-11 16:57:30 +0000

    Fix bug in findElf function to retrieve max calorie value carried by an elf

--- challenge1/main.go
+++ challenge1/main.go
@@ -9,51 +9,51 @@ import (
 )
 
 func main() {
-	var caloriesPerElf []int
-	inFile := os.Args[1:]
-	file, err := os.Open(inFile[0])
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	err = countCaloriesPerElf(file, caloriesPerElf)
+	caloriesPerElf, err := countCaloriesPerElf(file)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	file.Close()
-	fmt.Printf("Calories per elf: %q\n", caloriesPerElf)
-	fattestElf := findElfWithMostCalories(caloriesPerElf)
-	fmt.Printf("The fattest elf is elf number: %d\n", fattestElf)
+	fattestElfIdx := findElfWithMostCalories(caloriesPerElf)
+	fmt.Printf("The fattest elf is elf number: %d. He is carrying %d calories\n", fattestElfIdx+1, caloriesPerElf[fattestElfIdx])
 }
 
-func countCaloriesPerElf(F *os.File, caloriesPerElf []int) error {
-	scanner := bufio.NewScanner(F)
+func countCaloriesPerElf(F *os.File) ([]int, error) {
+	var caloriesPerElf []int
 	currentElfCalories := 0
+	scanner := bufio.NewScanner(F)
 	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "" {
+		line := scanner.Text()
+		if line == "" {
 			caloriesPerElf = append(caloriesPerElf, currentElfCalories)
 			currentElfCalories = 0
 			continue
 		}
-		calorieVal, err := strconv.Atoi(text)
+		calorieVal, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
-			return err
+			return nil, err
 		}
 		currentElfCalories += calorieVal
 	}
 
-	return nil
+	return caloriesPerElf, nil
 }
 
 func findElfWithMostCalories(caloriesPerElf []int) int {
-	max := 0
+	maxElf := 0
+	maxCalories := 0
 	for elf, elfCalories := range caloriesPerElf {
-		if elfCalories > max {
-			max = elf
+		if elfCalories > maxCalories {
+			maxElf = elf
+			maxCalories = elfCalories
 		}
 	}
-	return max
+	return maxElf
 }
