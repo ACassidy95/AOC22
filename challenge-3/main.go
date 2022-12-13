e635da2a65ca0388988384e29dcdf17f48274c49
aaronc <aaron.cassidy1995@gmail.com>
2022-12-13 22:28:36 +0000

    Add initial logic for challenge 3-2

--- challenge-3/main.go
+++ challenge-3/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -18,16 +17,102 @@ func main() {
 	prioritySum := prioritiseRucksacks(file)
 	file.Close()
 	fmt.Printf("Sum of item priorities: %d\n", prioritySum)
+
+	// Challenge 3-2
+	file, err = os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	prioritySum = prioritiseGroupBadges(file)
+	file.Close()
+	fmt.Printf("Sum of group badge priorities: %d\n", prioritySum)
 }
 
 func prioritiseRucksacks(F *os.File) int {
 	scanner := bufio.NewScanner(F)
 	prioritySum := 0
 	for scanner.Scan() {
-		rucksackContents := strings.Split(scanner.Text(), "")
+		rucksackContents := []byte(scanner.Text())
 		compartmentAContents := rucksackContents[:len(rucksackContents)/2]
 		compartmentBContents := rucksackContents[len(rucksackContents)/2:]
-		fmt.Printf("Compartment A: %x\nCompartment B: %x\n", compartmentAContents, compartmentBContents)
+
+		compartmentASet := contentsSet(compartmentAContents)
+		compartmentBSet := contentsSet(compartmentBContents)
+		compartmentSetIntersection := contentSetIntersection(compartmentASet, compartmentBSet)
+
+		for k := range compartmentSetIntersection {
+			prioritySum += calculateItemTypePriority(k)
+		}
 	}
 	return prioritySum
+}
+
+func prioritiseGroupBadges(F *os.File) int {
+	var currentGroup [][]byte
+	scanner := bufio.NewScanner(F)
+	prioritySum := 0
+	for scanner.Scan() {
+		rucksack := []byte(scanner.Text())
+		if len(rucksack) == 0 {
+			groupBadge := findGroupBadge(currentGroup)
+			prioritySum += calculateItemTypePriority(groupBadge)
+			currentGroup = nil
+			continue
+		}
+		currentGroup = append(currentGroup, rucksack)
+	}
+	return prioritySum
+}
+
+func findGroupBadge(rucksackGroup [][]byte) byte {
+	var groupBadge byte
+	rucksackGroupIntersection := make(map[byte]bool)
+
+	// Iteratively intersect sets constructed from the rucksack contents
+	// in order to find the common element across all rucksacks in group
+	for _, rucksack := range rucksackGroup {
+		rucksackSet := contentsSet(rucksack)
+		rucksackGroupIntersection = contentSetIntersection(rucksackGroupIntersection, rucksackSet)
+	}
+
+	// Iterate over intersection to assign badge value to var
+	// (it is expected this map will have only 1 element)
+	for k := range rucksackGroupIntersection {
+		groupBadge = k
+	}
+
+	return groupBadge
+}
+
+func contentsSet(contents []byte) map[byte]bool {
+	compartmentSet := make(map[byte]bool)
+	for _, item := range contents {
+		_, ok := compartmentSet[item]
+		if ok {
+			continue
+		}
+		compartmentSet[item] = true
+	}
+	return compartmentSet
+}
+
+func contentSetIntersection(c1, c2 map[byte]bool) map[byte]bool {
+	intersection := make(map[byte]bool)
+	for k := range c1 {
+		if c2[k] {
+			intersection[k] = true
+		}
+	}
+	return intersection
+}
+
+func calculateItemTypePriority(itemType byte) int {
+	itemValOffset := 0
+	if byte('A') <= itemType && itemType <= byte('Z') {
+		itemValOffset = 38
+	} else if byte('a') <= itemType && itemType <= byte('z') {
+		itemValOffset = 96
+	}
+	return int(itemType) - itemValOffset
 }
