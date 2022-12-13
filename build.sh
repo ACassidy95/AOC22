8e4300c4b31ea292bb1e9ccc0d7ee4408d4498cf
aaronc <aaron.cassidy1995@gmail.com>
2022-12-13 20:34:56 +0000

    Add simple build script

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+OPT_DELETE=0
+
+PROG_NAME=AOC22
+
+while getopts c:d flag
+do
+    case "${flag}" in
+        c) challenge=${OPTARG};;
+        d) OPT_DELETE=1;;
+    esac
+done
+
+build()
+{
+    echo "Copying source"
+    cp $challengeDir/main.go .
+
+    echo "Source copied"
+    echo "Building"
+
+    go mod init $PROG_NAME
+    go mod tidy
+    go build $PROG_NAME main.go
+    chmod u+x $PROG_NAME
+
+    echo "Build complete"
+}
+
+if [ $OPT_DELETE -eq 1 ]; then
+    echo "Deleting source and build artifacts"
+    rm $PROG_NAME
+    rm go.mod
+    rm main.go
+    echo "Clean-up complete"
+fi
+
+if ! [ -z ${challenge+x} ]; then
+    challengeDir=challenge-${challenge}
+    echo "Building $challengeDir"
+    build challengeDir
+fi
+
