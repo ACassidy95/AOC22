b36e6784cbe25541e35df55cf0f663d245fe2977
aaronc <aaron.cassidy1995@gmail.com>
2022-12-15 21:40:55 +0000

    Add rota processing and conversion to boundary value logic

--- challenge-4/main.go
+++ challenge-4/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,5 +22,33 @@ func main() {
 
 func findRedundantRotas(F *os.File) int {
 	redundantRotaCount := 0
+	scanner := bufio.NewScanner(F)
+	for scanner.Scan() {
+		rota := strings.Split(scanner.Text(), ",")
+		elfRotaA := rota[0]
+		elfRotaB := rota[1]
+		elfRotaALowerBound, elfRotaAUpperBound := convertRotaToIntBounds(elfRotaA)
+		elfRotaBLowerBound, elfRotaBUpperBound := convertRotaToIntBounds(elfRotaB)
+		totalOverlap := rotaTotallyRedundant(elfRotaALowerBound, elfRotaAUpperBound, elfRotaBLowerBound, elfRotaBUpperBound)
+		if totalOverlap {
+			redundantRotaCount++
+		}
+	}
 	return redundantRotaCount
+}
+
+func convertRotaToIntBounds(rota string) (int, int) {
+	rotaBoundInts := strings.Split(rota, "-")
+
+	// Don't Do What Donny Don't Does
+	// 1. Ignore errors
+	// 2. Trust input makers to not put in errors
+	rotaLowerBound, _ := strconv.Atoi(rotaBoundInts[0])
+	rotaUpperBound, _ := strconv.Atoi(rotaBoundInts[1])
+	return rotaLowerBound, rotaUpperBound
+}
+
+func rotaTotallyRedundant(lowerBoundA, upperBoundA, lowerBoundB, upperBoundB int) bool {
+	totalOverlap := true
+	return totalOverlap
 }
